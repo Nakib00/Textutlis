6c4cac6708826d7abd8d725dc21349337a1cf9f8
Nakib00 <2022142@iub.edu.bd>
2022-05-21 20:53:42 +0600

    if all select it's work

--- textutils/views.py
+++ textutils/views.py
@@ -27,47 +27,57 @@ def analyze(request):
                 analyzed = analyzed+char
 
         params = {'purpose': "Removing Punctuations", 'analyzed_text': analyzed}
-        return render(request, 'analyze.html',params)
+        # return render(request, 'analyze.html',params)
+        djtext=analyzed
 
 #it's work for make UpperCase
-    elif fullcaps=="on":
+    if fullcaps=="on":
         analyzed=""
         for char in djtext:
             analyzed=analyzed + char.upper()
 
         params = {'purpose': "Change to UpperCase", 'analyzed_text': analyzed}
-        return render(request, 'analyze.html',params)
+        # return render(request, 'analyze.html',params)
+        djtext=analyzed
 
 #it's work for New line Removing
-    elif newlineremover=="on":
+    if newlineremover=="on":
         analyzed=""
         for char in djtext:
             if char != "\n" and char!="\r":
                 analyzed=analyzed + char
 
         params = {'purpose': "Remove New Line", 'analyzed_text': analyzed}
-        return render(request, 'analyze.html',params)
+        # return render(request, 'analyze.html',params)
+        djtext=analyzed
 
 #it's work for Extra space remover'
-    elif extraspaceremover=="on":
+    if extraspaceremover=="on":
         analyzed=""
         for index, char in enumerate(djtext):
             if not(djtext[index]==" " and djtext[index+1]==" "):
                 analyzed=analyzed + char
 
         params = {'purpose': "Extra Space Remove", 'analyzed_text': analyzed}
-        return render(request, 'analyze.html',params)
+        # return render(request, 'analyze.html',params)
+        djtext=analyzed
 
 #it's work for Characters containing
-    elif charcounter=="on":
+    if charcounter=="on":
         count=0
         for index, char in enumerate(djtext):
             count+=1
 
         analyzed = count
         params = {'purpose': "Characters containing", 'analyzed_text': analyzed}
-        return render(request, 'analyze.html',params)
+        # return render(request, 'analyze.html',params)
+        djtext=analyzed
 
-#it's work for can't select any CheckBox
-    else:
-        return HttpResponse('Error POST')
+# #it's work for can't select any CheckBox
+#     else:
+#         return HttpResponse('Error POST')
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
